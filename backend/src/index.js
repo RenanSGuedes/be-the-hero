57a3ced5bb50e7d0a302c8ec3551fcb489a22e55
Renan Guedes <renanguedes.br@gmail.com>
2020-03-25 19:57:46 -0300

    front partial

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const routes = require('./routes')
 
 const url = new URL('https://127.0.0.1:5000/')
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
